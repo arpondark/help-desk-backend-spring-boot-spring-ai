3c58ce8b23775166a5b68c37777bd866a35b4426
MD. SHAZAN MAHMUD ARPON <51114057+arpondark@users.noreply.github.com>
2026-06-05 12:47:12 +0600

    memory integrated

--- src/main/java/site/shazan/helpdesk/help_desk_backend/config/AiConfig.java
+++ src/main/java/site/shazan/helpdesk/help_desk_backend/config/AiConfig.java
@@ -1,18 +1,27 @@
 package site.shazan.helpdesk.help_desk_backend.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AiConfig {
+    private Logger logger = LoggerFactory.getLogger(AiConfig.class);
 
     @Bean
-    public ChatClient chatClient(ChatClient.Builder builder) {
+    public ChatClient chatClient(ChatClient.Builder builder, ChatMemory chatMemory) {
+        logger.info("chatClient created");
+        logger.info("chat memory  created. {}", chatMemory.getClass().getName());
         return builder
                 .defaultSystem("Summerize the response within 400 words and make it as simple as possible")
-                .defaultAdvisors(new SimpleLoggerAdvisor())
+                .defaultAdvisors(new SimpleLoggerAdvisor(), MessageChatMemoryAdvisor.builder(chatMemory)
+                        .build())
                 .build();
     }
 }
